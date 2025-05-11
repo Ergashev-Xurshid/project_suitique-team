12a8a2b55e8b9076081294478de261e52a0c1cae
Muhammadumar <khilolamoon@gmail.com>
2025-05-11 13:30:21 +0500

    home page is ready

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import backgoundImage from "../../assets/homePage background image/Background.png";
+import { useTranslation } from "react-i18next";
 
 import { motion } from "framer-motion";
 import { FaArrowRight } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
 function Home() {
+  const { t, i18n } = useTranslation();
   const [data, setData] = useState([]);
-  const [images, setImage] = useState("");
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
 
   const getData = () => {
     fetch("https://back.aoron.uz/api/product")
@@ -57,7 +56,7 @@ function Home() {
           variants={containerVariants}
           initial="hidden"
           animate="visible"
-          className="absolute container top-1/3 left-[10%] text-white flex flex-col gap-5 justify-start items-start max-w-[90%] md:max-w-[50%]"
+          className="absolute top-1/3 left-[12%] xl:left-[3%] md:left-[12%] lg:left-[4%] max-sm:left-[5%] right-0 text-white flex flex-col gap-5 justify-start items-start w-full"
         >
           <motion.p
             variants={childVariants}
@@ -70,143 +69,141 @@ function Home() {
             variants={childVariants}
             className="text-7xl max-sm:text-5xl max-md:text-6xl max-lg:text-7xl font-light leading-tight"
           >
-            Премиальная <br /> мужская одежда
+            {t("homeTitle")}
           </motion.p>
 
           <motion.p
             variants={childVariants}
             className="text-base sm:text-lg md:text-xl text-[#EAEADC]"
           >
-            Качественные ткани. Идеальная посадка. <br /> Неподвластный времени
-            стиль.
+            {t("homeQuality")}
           </motion.p>
 
           <motion.button
             variants={childVariants}
             className="bg-white p-3 sm:p-4 text-black rounded-xl cursor-pointer flex items-center gap-2 group text-sm sm:text-base"
           >
-            <p>Смотреть коллекцию</p>
+            <p>{t("exploreCollection")}</p>
             <FaArrowRight className="transform transition-transform duration-300 group-hover:translate-x-2" />
           </motion.button>
         </motion.div>
       </section>
       <div className="container mx-auto px-4 sm:px-8 lg:px-12">
         <p className="text-center text-4xl font-light mt-15">
-          Рекомендуемые товары
+          <p>{t("featuredProducts")}</p>
         </p>
         <p className="text-center mt-5 text-gray-600">
-          Откройте для себя нашу тщательно отобранную коллекцию мужской одежды
-          <br />
-          премиум-класса, изготовленную из лучших материалов с вниманием к
-          деталям.
+          {t("featuredProductsSubtitle")}
         </p>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-10">
           {data.map((element, index) => (
-            <Link to={`/product/${element.id}`} key={index}>
-              <div className="flex flex-col gap-2">
-                <div className="relative group overflow-hidden cursor-pointer">
-                  {element.images?.length > 0 && (
-                    <img
-                      src={`https://back.aoron.uz/${element.images[0]}`}
-                      alt="image"
-                      className="w-full h-auto transform transition-transform duration-500 group-hover:scale-150"
-                    />
-                  )}
-                </div>
-                <div className="flex justify-between items-center">
-                  <p className="font-semibold">{element.title_en}</p>
-                  <p>{`$${element.price}`}</p>
-                </div>
-                <p className="text-gray-500">
-                  {element.description_en.length > 99
-                    ? element.description_en.slice(
+            <div key={index} className="flex flex-col gap-2">
+              <div className="relative group overflow-hidden cursor-pointer">
+                {element.images?.length > 0 && (
+                  <img
+                    src={`https://back.aoron.uz/${element.images[0]}`}
+                    alt="image"
+                    className="w-full h-auto transform transition-transform duration-500 group-hover:scale-150"
+                  />
+                )}
+              </div>
+              <div className="flex justify-between items-center">
+                <p className="font-semibold">{element.title_en}</p>
+                <p>{`$${element.price}`}</p>
+              </div>
+              <p className="text-gray-500">
+                {element.description_en.length > 99
+                  ? element.description_en.slice(
                       0,
                       element.description_en.lastIndexOf(" ", 100)
                     ) + "..."
-                    : element.description_en}
-                </p>
-                <div className="flex gap-2 mt-2">
-                  {element.colors?.map((color, i) => (
-                    <div
-                      key={i}
-                      className="w-3 h-3 rounded-full border"
-                      style={{ backgroundColor: color.color_en }}
-                      title={color.color_en}
-                    ></div>
-                  ))}
-                </div>
+                  : element.description_en}
+              </p>
+              <div className="flex gap-2 mt-2">
+                {element.colors?.map((color, i) => (
+                  <div
+                    key={i}
+                    className="w-3 h-3 rounded-full border"
+                    style={{ backgroundColor: color.color_en }}
+                    title={color.color_en}
+                  ></div>
+                ))}
               </div>
-            </Link>
+            </div>
           ))}
         </div>
-        <motion.button
-          variants={childVariants}
-          className="bg-white p-3 sm:p-4 border border-gray-200 mx-auto text-black rounded-xl cursor-pointer flex items-center gap-2 group text-sm sm:text-base"
-        >
-          <p>Посмотреть все продукты</p>
-          <FaArrowRight className="transform transition-transform duration-300 group-hover:translate-x-2" />
-        </motion.button>
-        <div className="flex items-center justify-between w-full mt-10">
-          <p className="text-4xl font-light">Новинки</p>
+        <Link to="/catalog">
           <motion.button
             variants={childVariants}
-            className="bg-white p-3 sm:p-4 border border-gray-200 text-black rounded-xl cursor-pointer flex items-center gap-2 group text-sm sm:text-base"
+            className="bg-white p-3 sm:p-4 border border-gray-200 mx-auto text-black rounded-xl cursor-pointer flex items-center gap-2 group text-sm sm:text-base"
           >
-            <p>Посмотреть все продукты</p>
+            {t("viewAllProducts")}
             <FaArrowRight className="transform transition-transform duration-300 group-hover:translate-x-2" />
           </motion.button>
+        </Link>
+        <div className="flex items-center justify-between w-full mt-10">
+          <p className="text-4xl font-light">{t("newArrivals")}</p>
+          <Link to="/catalog">
+            <motion.button
+              variants={childVariants}
+              className="bg-white p-3 sm:p-4 border border-gray-200 text-black rounded-xl cursor-pointer flex items-center gap-2 group text-sm sm:text-base"
+            >
+              {t("viewAllProducts")}
+              <FaArrowRight className="transform transition-transform duration-300 group-hover:translate-x-2" />
+            </motion.button>
+          </Link>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-10">
           {data.slice(0, 4).map((element, index) => (
-            <Link to={`/product/${element.id}`}>
-              <div key={index} className="flex flex-col gap-2">
-                <div className="relative group overflow-hidden cursor-pointer">
-                  {element.images?.length > 0 && (
-                    <img
-                      src={`https://back.aoron.uz/${element.images[0]}`}
-                      alt="image"
-                      className="w-full h-auto transform transition-transform duration-500 group-hover:scale-150"
-                    />
-                  )}
-                  <p className="bg-black px-3 py-1 text-sm text-white absolute top-2">
-                    NEW
-                  </p>
-                </div>
-                <div className="flex justify-between items-center">
-                  <p className="font-semibold">{element.title_en}</p>
-                  <p>{`$${element.price}`}</p>
-                </div>
-                <p className="text-gray-500">
-                  {element.description_en.length > 99
-                    ? element.description_en.slice(
+            <div key={index} className="flex flex-col gap-2">
+              <div className="relative group overflow-hidden cursor-pointer">
+                {element.images?.length > 0 && (
+                  <img
+                    src={`https://back.aoron.uz/${element.images[0]}`}
+                    alt="image"
+                    className="w-full h-auto transform transition-transform duration-500 group-hover:scale-150"
+                  />
+                )}
+                <p className="bg-black px-3 py-1 text-sm text-white absolute top-2">
+                  NEW
+                </p>
+              </div>
+              <div className="flex justify-between items-center">
+                <p className="font-semibold">{element.title_en}</p>
+                <p>{`$${element.price}`}</p>
+              </div>
+              <p className="text-gray-500">
+                {element.description_en.length > 99
+                  ? element.description_en.slice(
                       0,
                       element.description_en.lastIndexOf(" ", 100)
                     ) + "..."
-                    : element.description_en}
-                </p>
-                <div className="flex gap-2 mt-2">
-                  {element.colors?.map((color, i) => (
-                    <div
-                      key={i}
-                      className="w-3 h-3 rounded-full border"
-                      style={{ backgroundColor: color.color_en }}
-                      title={color.color_en}
-                    ></div>
-                  ))}
-                </div>
+                  : element.description_en}
+              </p>
+              <div className="flex gap-2 mt-2">
+                {element.colors?.map((color, i) => (
+                  <div
+                    key={i}
+                    className="w-3 h-3 rounded-full border"
+                    style={{ backgroundColor: color.color_en }}
+                    title={color.color_en}
+                  ></div>
+                ))}
               </div>
-            </Link>
+            </div>
           ))}
         </div>
         <div className="flex items-center justify-between w-full my-20">
-          <p className="text-4xl font-light">Распродажа</p>
-          <motion.button
-            variants={childVariants}
-            className="bg-white p-3 sm:p-4 border border-gray-200 text-black rounded-xl cursor-pointer flex items-center gap-2 group text-sm sm:text-base"
-          >
-            <p>Посмотреть все продукты</p>
-            <FaArrowRight className="flex transform transition-transform duration-300 group-hover:translate-x-2" />
-          </motion.button>
+          <p className="text-4xl font-light">{t("sale")}</p>
+          <Link to="/catalog">
+            <motion.button
+              variants={childVariants}
+              className="bg-white p-3 sm:p-4 border border-gray-200 text-black rounded-xl cursor-pointer flex items-center gap-2 group text-sm sm:text-base"
+            >
+              {t("viewAllProducts")}
+              <FaArrowRight className="flex transform transition-transform duration-300 group-hover:translate-x-2" />
+            </motion.button>
+          </Link>
         </div>
       </div>
     </div>
